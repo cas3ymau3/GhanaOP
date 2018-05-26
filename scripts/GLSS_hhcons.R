cafd728f94f015e04fba702da0165572f129cf4c
cas3ymau3 <cmauee@gmail.com>
2018-05-25 18:12:33 -0700

    transferred `relevant files' commentary to text file

--- scripts/GLSS_hhcons.R
+++ scripts/GLSS_hhcons.R
@@ -1,38 +1,3 @@
 # Description: The purpose of this code is to extract the household consumption aggregate
 # data from each of the 7 waves of the GLSS, and convert these into a repeated `admin1` 
 # level cross-sectional dataset
-
-
-# Relevant files: 
-
-# Consumption Aggregates 
-  # Wave 1: 
-    # EXPEND.DTA 
-  # Wave 2: 
-    # EXPEND.DTA 
-  # Wave 3: 
-    # AGG1-12, SUBAGG1-40, EXP1-21, INC1-16
-    # POV_GH.DTA 
-  # Wave 4: 
-    # AGG1-12, SUBAGG1-40, EXP1-21, INC1-16
-    # POV_GH.DTA 
-  # wave 5-1 
-    # `Stata8'`
-      # 01_GHA_EXPFOOD - 12_GHA_EXPMISC
-    # GLSS5-Data 
-      # agg1-12, e1-21, ee1-21, eee1-21,
-      # exp1-21, i1-16, ii1-16, iii1-16, inc1-16
-      # pov_gh5.dta
-  # wave 5-2 
-    # none really 
-  # wave 6 
-    # `AGGREGATES' folder 
-      # 01_GHA_EXPFOOD - 012_HA_EXPMISC
-    # GHA_2013_INCOME 
-    # INC_AGRI to INCfromSec11c 
-    # POV_GHA_2013_GLSS6-updated 
-
-
-# Community Datasets 
-
-# 
